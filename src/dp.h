8e89bf7f69c9d5ed66e205113785a68e5aa55b30
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-03 13:32:50 +1100

    trim dp.h

--- src/dp.h
+++ src/dp.h
@@ -1,16 +1,4 @@
-#ifdef enablemultithread
-#define TLS __thread
-#else
-#define TLS
-#endif
-
-#ifdef enableatomic
-#define ATOMICINT atomic_int
-#else
-#define ATOMICINT int
-#endif
-
-extern TLS int   commonAlloc1;
-extern TLS int   commonAlloc2;
-extern TLS int** commonIP;
-extern TLS int** commonJP;
+extern int   commonAlloc1;
+extern int   commonAlloc2;
+extern int** commonIP;
+extern int** commonJP;
