f1ddfb20843c14ceb385f2ed4a42109a8d8f4e0c
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-05 14:56:11 +1100

    improve assert

--- src/mltaln.h
+++ src/mltaln.h
@@ -21,8 +21,58 @@
 #endif
 
 // clang-format off
+
+// Taken from portable snippets
+// https://github.com/nemequ/portable-snippets/blob/master/debug-trap/debug-trap.h
+#if defined(__has_builtin) && !defined(__ibmxl__)
+#if __has_builtin(__builtin_debugtrap)
+#define debugbreak() __builtin_debugtrap()
+#elif __has_builtin(__debugbreak)
+#define debugbreak() __debugbreak()
+#endif
+#endif
+#if !defined(debugbreak)
+#if defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#define debugbreak() __debugbreak()
+#elif defined(__ARMCC_VERSION)
+#define debugbreak() __breakpoint(42)
+#elif defined(__ibmxl__) || defined(__xlC__)
+#include <builtins.h>
+#define debugbreak() __trap(42)
+#elif defined(__DMC__) && defined(_M_IX86)
+#define debugbreak() __asm int 3h;
+#elif defined(__i386__) || defined(__x86_64__)
+#define debugbreak() __asm__ __volatile__("int3")
+#elif defined(__thumb__)
+#define debugbreak() __asm__ __volatile__(".inst 0xde01")
+#elif defined(__aarch64__)
+#define debugbreak() __asm__ __volatile__(".inst 0xd4200000")
+#elif defined(__arm__)
+#define debugbreak() __asm__ __volatile__(".inst 0xe7f001f0")
+#elif defined (__alpha__) && !defined(__osf__)
+#define debugbreak() __asm__ __volatile__("bpt")
+#elif defined(_54_)
+#define debugbreak() __asm__ __volatile__("ESTOP")
+#elif defined(_55_)
+#define debugbreak() __asm__ __volatile__(";\n .if (.MNEMONIC)\n ESTOP_1\n .else\n ESTOP_1()\n .endif\n NOP")
+#elif defined(_64P_)
+#define debugbreak() __asm__ __volatile__("SWBP 0")
+#elif defined(_6x_)
+#define debugbreak() __asm__ __volatile__("NOP\n .word 0x10000000")
+#elif defined(__STDC_HOSTED__) && (__STDC_HOSTED__ == 0) && defined(__GNUC__)
+#define debugbreak() __builtin_trap()
+#else
+#include <signal.h>
+#if defined(SIGTRAP)
+#define debugbreak() raise(SIGTRAP)
+#else
+#define debugbreak() raise(SIGABRT)
+#endif
+#endif
+#endif
+
 #ifndef assertAction
-#define assertAction() do {_exit(1);} while (0)
+#define assertAction() do {reporterr("assertion failure at %s:%d\n", __FILE__, __LINE__); debugbreak(); _exit(1);} while (0)
 #endif
 
 #ifndef assert
