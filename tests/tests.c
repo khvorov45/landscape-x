56db864d2b47d9ed00f2244f39e46e85bb47c6ae
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-12 15:46:52 +1100

    use prb rng

--- tests/tests.c
+++ tests/tests.c
@@ -8,37 +8,6 @@ typedef uint32_t u32;
 typedef int32_t  i32;
 typedef uint8_t  u8;
 
-typedef struct Rng {
-    u64 state;
-    u64 inc;
-} Rng;
-
-function u32
-getRandomU32(Rng* rng) {
-    u64 oldstate = rng->state;
-    rng->state = oldstate * 6364136223846793005ULL + rng->inc;
-    u32 xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
-    u32 rot = oldstate >> 59u;
-    u32 result = (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
-    return result;
-}
-
-function Rng
-createRng(u64 initstate, u64 initseq) {
-    Rng rng = {.state = 0, .inc = (initseq << 1u) | 1u};
-    getRandomU32(&rng);
-    rng.state += initstate;
-    getRandomU32(&rng);
-    return rng;
-}
-
-function u32
-getRandomU32BiasedBound(Rng* rng, i32 bound) {
-    u32 randU32 = getRandomU32(rng);
-    u32 result = randU32 % bound;
-    return result;
-}
-
 typedef struct GenerateSequencesResult {
     prb_Str  full;
     prb_Str* seqs;
@@ -46,10 +15,10 @@ typedef struct GenerateSequencesResult {
 } GenerateSequencesResult;
 
 function GenerateSequencesResult
-generateSequences(prb_Arena* arena, Rng* rng, char* choices, i32 choicesCount, i32 lengthFullSeq, i32 seqCount) {
+generateSequences(prb_Arena* arena, prb_Rng* rng, char* choices, i32 choicesCount, i32 lengthFullSeq, i32 seqCount) {
     char* fullSeqBuf = prb_arenaAllocArray(arena, char, lengthFullSeq + 1);
     for (i32 aaIndex = 0; aaIndex < lengthFullSeq; aaIndex++) {
-        u32  choiceIndex = getRandomU32BiasedBound(rng, choicesCount);
+        u32  choiceIndex = prb_randomU32Bound(rng, choicesCount);
         char choice = choices[choiceIndex];
         fullSeqBuf[aaIndex] = choice;
     }
@@ -62,12 +31,12 @@ generateSequences(prb_Arena* arena, Rng* rng, char* choices, i32 choicesCount, i
 
     prb_Str* seqs = prb_arenaAllocArray(arena, prb_Str, seqCount);
     for (i32 seqIndex = 0; seqIndex < seqCount; seqIndex++) {
-        i32 startIndex = getRandomU32BiasedBound(rng, maxTrimFromEnds + 1);
-        i32 onePastEndIndex = lengthFullSeq - getRandomU32BiasedBound(rng, maxTrimFromEnds + 1);
+        i32 startIndex = prb_randomU32Bound(rng, maxTrimFromEnds + 1);
+        i32 onePastEndIndex = lengthFullSeq - prb_randomU32Bound(rng, maxTrimFromEnds + 1);
         i32 seqLen = onePastEndIndex - startIndex;
-        i32 mutationCount = getRandomU32BiasedBound(rng, maxMutations + 1);
+        i32 mutationCount = prb_randomU32Bound(rng, maxMutations + 1);
         for (i32 mutationIndex = 0; mutationIndex < mutationCount; mutationIndex++) {
-            mutationBuffer[mutationIndex] = getRandomU32BiasedBound(rng, seqLen);
+            mutationBuffer[mutationIndex] = prb_randomU32Bound(rng, seqLen);
         }
 
         char* seqBuf = prb_arenaAllocArray(arena, char, onePastEndIndex - startIndex + 1);
@@ -80,7 +49,7 @@ generateSequences(prb_Arena* arena, Rng* rng, char* choices, i32 choicesCount, i
             }
 
             if (isMutated) {
-                u32  choiceIndex = getRandomU32BiasedBound(rng, choicesCount);
+                u32  choiceIndex = prb_randomU32Bound(rng, choicesCount);
                 char choice = choices[choiceIndex];
                 seqBuf[seqIndex] = choice;
             } else {
@@ -89,7 +58,7 @@ generateSequences(prb_Arena* arena, Rng* rng, char* choices, i32 choicesCount, i
         }
         seqBuf[seqLen] = '\0';
 
-        i32 xLocation = getRandomU32BiasedBound(rng, seqLen);
+        i32 xLocation = prb_randomU32Bound(rng, seqLen);
         seqBuf[xLocation] = 'X';
 
         seqs[seqIndex] = (prb_Str) {seqBuf, seqLen};
@@ -149,8 +118,8 @@ main() {
     prb_TimeStart testsStart = prb_timeStart();
     prb_Arena     arena_ = prb_createArenaFromVmem(1 * prb_GIGABYTE);
     prb_Arena*    arena = &arena_;
-    Rng           rng_ = createRng(1, 3);
-    Rng*          rng = &rng_;
+    prb_Rng       rng_ = prb_createRng(0);
+    prb_Rng*      rng = &rng_;
     prb_Str       testsDir = prb_getParentDir(arena, prb_STR(__FILE__));
     prb_Str       rootDir = prb_getParentDir(arena, testsDir);
 
@@ -201,8 +170,8 @@ main() {
     prb_assert(prb_clearDir(arena, tempDir));
     prb_Str cwd = prb_getWorkingDir(arena);
     prb_assert(prb_setWorkingDir(arena, tempDir));
-    intptr_t outBytes = prb_arenaFreeSize(arena);
-    void*    outBuf = prb_arenaFreePtr(arena);
+    intptr_t        outBytes = prb_arenaFreeSize(arena);
+    void*           outBuf = prb_arenaFreePtr(arena);
     aln_AlignResult myAlignResult = tbfast_main((aln_Str*)genSeq.seqs, genSeq.seqCount, outBuf, outBytes, aln_defaultOpts());
     prb_arenaChangeUsed(arena, myAlignResult.bytesWritten);
     prb_assert(prb_setWorkingDir(arena, cwd));
