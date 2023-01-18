c5da587dce5b7629f6b1cb31981467c319b57830
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-18 12:01:49 +1100

    inline constants

--- src/tbfast_lib.c
+++ src/tbfast_lib.c
@@ -91,11 +91,190 @@ tbfast_main(aln_Str* strings, intptr_t stringsCount, void* out, intptr_t outByte
         }
     }
 
-    constants(opts, ctx);
+    {
+        {
+            ctx->n_dis = AllocateIntMtx(ctx->nalphabets, ctx->nalphabets);
+            ctx->n_disLN = AllocateDoubleMtx(ctx->nalphabets, ctx->nalphabets);
+            double** n_distmp = AllocateDoubleMtx(20, 20);
+            double*  datafreq = AllocateDoubleVec(20);
+            double*  freq = AllocateDoubleVec(20);
+
+            aln_assert(opts.ppenalty_dist != NOTSPECIFIED);
+            aln_assert(opts.ppenalty_ex != NOTSPECIFIED);
+
+            if (ctx->ppenalty_OP == NOTSPECIFIED)
+                ctx->ppenalty_OP = -1530;
+            if (ctx->ppenalty_EX == NOTSPECIFIED)
+                ctx->ppenalty_EX = -00;
+            if (ctx->pamN == NOTSPECIFIED)
+                ctx->pamN = 0;
+            if (ctx->kimuraR == NOTSPECIFIED)
+                ctx->kimuraR = 1;
+            ctx->penalty_dist = (int)(0.6 * opts.ppenalty_dist + 0.5);
+            ctx->penalty_OP = (int)(0.6 * ctx->ppenalty_OP + 0.5);
+            ctx->penalty_ex = (int)(0.6 * opts.ppenalty_ex + 0.5);
+            ctx->penalty_EX = (int)(0.6 * ctx->ppenalty_EX + 0.5);
+            ctx->offsetFFT = (int)(0.6 * (-0) + 0.5);
+            ctx->offsetLN = (int)(0.6 * 100 + 0.5);
+            ctx->penaltyLN = (int)(0.6 * -2000 + 0.5);
+            ctx->penalty_exLN = (int)(0.6 * -100 + 0.5);
+
+            {
+                char   locaminod[] = "ARNDCQEGHILKMFPSTWYVBZX.-J";
+                char   locgrpd[] = {0, 3, 2, 2, 5, 2, 2, 0, 3, 1, 1, 3, 1, 4, 0, 0, 0, 4, 4, 1, 2, 2, 6, 6, 6, 1};
+                double freqd[20] = {0.077, 0.051, 0.043, 0.052, 0.020, 0.041, 0.062, 0.074, 0.023, 0.052, 0.091, 0.059, 0.024, 0.040, 0.051, 0.069, 0.059, 0.014, 0.032, 0.066};
+
+                // clang-format off
+                // NOTE(sen) BLOSUM62
+                double tmpmtx[] = { 
+                    5.893685,
+                    -2.120252,  8.210189,
+                    -2.296072, -0.659672,  8.479856,
+                    -2.630151, -2.408668,  1.907550,  8.661363,
+                    -0.612761, -5.083814, -3.989626, -5.189966, 12.873172,
+                    -1.206025,  1.474162,  0.002529, -0.470069, -4.352838,  7.927704,
+                    -1.295821, -0.173087, -0.402015,  2.265459, -5.418729,  2.781955,  7.354247,
+                    0.239392, -3.456163, -0.634136, -1.970281, -3.750621, -2.677743, -3.165266,  8.344902,
+                    -2.437724, -0.374792,  0.867735, -1.678363, -4.481724,  0.672051, -0.176497, -3.061315, 11.266586,
+                    -1.982718, -4.485360, -4.825558, -4.681732, -1.841495, -4.154454, -4.791538, -5.587336, -4.847345,  5.997760,
+                    -2.196882, -3.231860, -5.068375, -5.408471, -1.916207, -3.200863, -4.269723, -5.440437, -4.180099,  2.282412,  5.774148,
+                    -1.101017,  3.163105, -0.268534, -1.052724, -4.554510,  1.908859,  1.163010, -2.291924, -1.081539, -4.005209, -3.670219,  6.756827,
+                    -1.402897, -2.050705, -3.226290, -4.587785, -2.129758, -0.631437, -2.997038, -4.014898, -2.326896,  1.690191,  2.987638, -2.032119,  8.088951,
+                    -3.315080, -4.179521, -4.491005, -5.225795, -3.563219, -4.746598, -4.788639, -4.661029, -1.851231, -0.241317,  0.622170, -4.618016,  0.018880,  9.069126,
+                    -1.221394, -3.162863, -3.000581, -2.220163, -4.192770, -1.922917, -1.674258, -3.200320, -3.241363, -4.135001, -4.290107, -1.520445, -3.714633, -5.395930, 11.046892,
+                    1.673639, -1.147170,  0.901353, -0.391548, -1.312485, -0.151708, -0.220375, -0.438748, -1.322366, -3.522266, -3.663923, -0.305170, -2.221304, -3.553533, -1.213470,  5.826527,
+                    -0.068042, -1.683495, -0.069138, -1.576054, -1.299983, -1.012997, -1.294878, -2.363065, -2.528844, -1.076382, -1.796229, -1.004336, -0.999449, -3.161436, -1.612919,  2.071710,  6.817956,
+                    -3.790328, -4.019108, -5.543911, -6.321502, -3.456164, -2.919725, -4.253197, -3.737232, -3.513238, -3.870811, -2.447829, -4.434676, -2.137255,  1.376341, -5.481260, -4.127804, -3.643382, 15.756041,
+                    -2.646022, -2.540799, -3.122641, -4.597428, -3.610671, -2.131601, -3.030688, -4.559647,  2.538948, -1.997058, -1.593097, -2.730047, -1.492308,  4.408690, -4.379667, -2.528713, -2.408996,  3.231335,  9.892544,
+                    -0.284140, -3.753871, -4.314525, -4.713963, -1.211518, -3.297575, -3.663425, -4.708118, -4.676220,  3.820569,  1.182672, -3.393535,  1.030861, -1.273542, -3.523054, -2.469318, -0.083276, -4.251392, -1.811267,  5.653391
+                };
+                // clang-format on
+
+                int count = 0;
+                for (int i = 0; i < 20; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        n_distmp[i][j] = n_distmp[j][i] = (double)tmpmtx[count++];
+                    }
+                }
+
+                for (int i = 0; i < 20; i++) {
+                    freq[i] = freqd[i];
+                }
+
+                for (int i = 0; i < 26; i++)
+                    ctx->amino[i] = locaminod[i];
+                for (int i = 0; i < 26; i++)
+                    ctx->amino_grp[(int)ctx->amino[i]] = locgrpd[i];
+            }
+
+            for (int i = 0; i < 0x80; i++)
+                ctx->amino_n[i] = -1;
+            for (int i = 0; i < 26; i++)
+                ctx->amino_n[(int)ctx->amino[i]] = i;
+
+            double* freq1 = freq;
+
+            double average = 0.0;
+            {
+                for (int i = 0; i < 20; i++)
+                    for (int j = 0; j < 20; j++)
+                        average += n_distmp[i][j] * freq1[i] * freq1[j];
+            }
+
+            {
+                for (int i = 0; i < 20; i++)
+                    for (int j = 0; j < 20; j++)
+                        n_distmp[i][j] -= average;
+            }
+
+            average = 0.0;
+            for (int i = 0; i < 20; i++)
+                average += n_distmp[i][i] * freq1[i];
+
+            {
+                for (int i = 0; i < 20; i++)
+                    for (int j = 0; j < 20; j++)
+                        n_distmp[i][j] *= 600.0 / average;
+            }
+
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    n_distmp[i][j] -= opts.offset;
+
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    int out = 0;
+                    {
+                        int in = n_distmp[i][j];
+                        if (in > 0.0)
+                            out = ((int)(in + 0.5));
+                        else if (in == 0.0)
+                            out = (0);
+                        else if (in < 0.0)
+                            out = ((int)(in - 0.5));
+                        else
+                            out = 0;
+                    }
+                    n_distmp[i][j] = out;
+                }
+            }
+
+            for (int i = 0; i < 26; i++)
+                for (int j = 0; j < 26; j++)
+                    ctx->n_dis[i][j] = 0;
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    ctx->n_dis[i][j] = (int)n_distmp[i][j];
+
+            FreeDoubleMtx(n_distmp);
+            FreeDoubleVec(datafreq);
+            FreeDoubleVec(freq);
+        }
+
+        int charsize = 0x80;
+        ctx->amino_dis = AllocateIntMtx(charsize, charsize);
+
+        for (int i = 0; i < charsize; i++)
+            ctx->amino_n[i] = -1;
+
+        for (int i = 0; i < ctx->nalphabets; i++)
+            ctx->amino_n[(int)ctx->amino[i]] = i;
+
+        for (int i = 0; i < charsize; i++)
+            for (int j = 0; j < charsize; j++)
+                ctx->amino_dis[i][j] = 0;
+
+        for (int i = 0; i < ctx->nalphabets; i++)
+            for (int j = 0; j < ctx->nalphabets; j++)
+                ctx->n_disLN[i][j] = 0;
+
+        ctx->n_dis_consweight_multi = AllocateDoubleMtx(ctx->nalphabets, ctx->nalphabets);
+        ctx->n_disFFT = AllocateIntMtx(ctx->nalphabets, ctx->nalphabets);
+        for (int i = 0; i < ctx->nalphabets; i++)
+            for (int j = 0; j < ctx->nalphabets; j++) {
+                ctx->amino_dis[(int)ctx->amino[i]][(int)ctx->amino[j]] = ctx->n_dis[i][j];
+                ctx->n_dis_consweight_multi[i][j] = (double)ctx->n_dis[i][j] * ctx->consweight_multi;
+            }
+
+        {
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    ctx->n_disLN[i][j] = (double)ctx->n_dis[i][j] + opts.offset - ctx->offsetLN;
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    ctx->n_disFFT[i][j] = ctx->n_dis[i][j] + opts.offset - ctx->offsetFFT;
+        }
+
+        if (ctx->fftThreshold == NOTSPECIFIED) {
+            ctx->fftThreshold = FFT_THRESHOLD;
+        }
+        if (ctx->fftWinSize == NOTSPECIFIED) {
+            ctx->fftWinSize = FFT_WINSIZE_P;
+        }
+    }
 
     {
         aln_assert(ctx->njob >= 2);
-
 
         // TODO(sen) Sequence verification?
 
